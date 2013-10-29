2addf938ff8177d932ea90990304e75eb5829f73
cfalzone <cfalzone@aquent.com>
2013-10-29 11:31:02 -0400

    First OSGI try

---
+++ src/com/aquent/viewtools/MyToolInfo.java
@@ -0,0 +1,34 @@
+package com.aquent.viewtools;
+
+import org.apache.velocity.tools.view.context.ViewContext;
+import org.apache.velocity.tools.view.servlet.ServletToolInfo;
+
+public class MyToolInfo extends ServletToolInfo {
+
+    @Override
+    public String getKey () {
+        return "twitter";
+    }
+
+    @Override
+    public String getScope () {
+        return ViewContext.APPLICATION;
+    }
+
+    @Override
+    public String getClassname () {
+        return TwitterTool.class.getName();
+    }
+
+    @Override
+    public Object getInstance ( Object initData ) {
+
+        TwitterTool viewTool = new TwitterTool();
+        viewTool.init( initData );
+
+        setScope( ViewContext.APPLICATION );
+
+        return viewTool;
+    }
+
+}
